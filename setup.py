74f9031239b33cf060356b8ce8596249cb53104f
Ziyad Edher <ziyad.edher@gmail.com>
2019-03-21 16:14:12 -0400

    Introduce initial setuptool configuration files

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""Simple setuptools pip setup script.
+
+Reads from `setup.cfg` for configuration.
+Note this needs Python 3.6 or above to run correctly.
+"""
+
+from setuptools import setup  # type: ignore
+
+setup()
